ed173497db99bc725b25ee75bb728303c60dd455
BakharEugene <eugenebakhar@gmail.com>
2016-05-31 12:21:31 +0300

    Final Solution

---
+++ mvc course work/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using mvc_course_work.Models.Entities;
+using System.Text;
+
+namespace mvc_course_work.Controllers
+{
+    public class ChatController:Controller
+    {
+        [Authorize(Roles = "Admin, User")]
+        public ActionResult Chat(mvc_course_work.Models.ViewModel.ChatModel model)
+        {
+           
+            return View(model);
+        }
+
+    }
+}
+
---
+++ mvc course work/Models/ViewModel/ChatModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_course_work.Models.ViewModel
+{
+    public class ChatModel
+    {
+    }
+}
